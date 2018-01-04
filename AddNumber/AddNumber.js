e0171b747222340ea0ffbc99f03af69871bd27dc
Sohini Makar <sohini.makar@gmail.com>
2018-01-03 20:45:12 -0800

    Add files via upload

---
+++ AddNumber/AddNumber.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+export default class AddNumber extends React.Component{
+	constructor(props){
+  super(props)
+  this.state = {counter : 0}
+  }
+  
+  handleClick(val){
+   this.setState({
+   	counter : this.state.counter + val
+   })
+  }
+  
+  render() {
+  return (
+  <div>
+  	<div>
+  	<button onClick={this.handleClick.bind(this, 1)}>Add 1</button>
+    <button onClick={this.handleClick.bind(this, 5)}>Add 5</button><br/>
+    </div>
+    <input value={this.state.counter}/>
+  </div>
+  )
+  }
+}
+
+class App extends React.Component{
+	render(){
+  	return (
+    <div>
+    	<AddNumber/>
+    </div>
+    )
+  }
+}
+
